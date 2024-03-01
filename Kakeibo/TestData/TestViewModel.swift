6bb83d1fe72edff6358fb3e0cef887507436a5a1
me990928 <me990928@gmail.com>
2024-03-01 17:12:00 +0900

    testデータを制御するVMの作成

--- Kakeibo/TestData/TestViewModel.swift
+++ Kakeibo/TestData/TestViewModel.swift
@@ -29,60 +29,10 @@ class TestViewModel: ObservableObject {
         self.model.kakeiboLargeSmallCategory.append(KakeiboLargeSmallCategory(id: "c5", kakeiboId: "case5", smallId: "sc1", largeId: "lc1"))
         self.model.kakeiboLargeSmallCategory.append(KakeiboLargeSmallCategory(id: "c6", kakeiboId: "case5", smallId: "sc5", largeId: "lc1"))
         
-        self.model.kakeiboModel.append(KakeiboModel(id: "case1", name: "セブンイレブン弁当", price: 500, createAt: changeStringDate("2024/01/20 11:21:33"), updateAt: changeStringDate("2024/01/20 11:21:33"), categoryId: "c1", check: false))
-        self.model.kakeiboModel.append(KakeiboModel(id: "case2", name: "ファミマ弁当", price: 300, createAt: changeStringDate("2024/02/20 11:22:33"), updateAt: changeStringDate("2024/02/20 11:22:33"), categoryId: "c2", check: false))
-        self.model.kakeiboModel.append(KakeiboModel(id: "case3", name: "白菜", price: 100, createAt: changeStringDate("2024/02/20 11:23:33"), updateAt: changeStringDate("2024/02/20 11:23:33"), categoryId: "c3", check: false))
-        self.model.kakeiboModel.append(KakeiboModel(id: "case4", name: "セブンイレブン弁当", price: 500, createAt: changeStringDate("2024/03/20 11:24:33"), updateAt: changeStringDate("2024/03/21 11:24:33"), categoryId: "c4", check: false))
-        self.model.kakeiboModel.append(KakeiboModel(id: "case5", name: "フレンチレストラン", price: 5000, createAt: changeStringDate("2024/03/20 11:25:33"), updateAt: changeStringDate("2024/03/20 11:25:33"), categoryId: "c5", check: false))
-    }
-    
-    func changeStringDate(_ dateString: String, _ dateFormatString: String = "yyyy/MM/dd HH:mm:ss")->Date{
-        
-        let dateFormatter = DateFormatter()
-        
-        // フォーマット設定
-        dateFormatter.dateFormat = dateFormatString
-
-        // ロケール設定（端末の暦設定に引きづられないようにする）
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-
-        // タイムゾーン設定（端末設定によらず、どこの地域の時間帯なのかを指定する）
-        dateFormatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
-
-        // 変換
-//        let date = dateFormatter.date(from: "2020/10/20 11:22:33")
-        let date = dateFormatter.date(from: dateString)
-
-        // 結果表示
-        print(date!) // -> 2020-10-20 02:22:33 +0000
-        
-        if let formatedDate = date {
-            return formatedDate
-        } else {
-            return dateFormatter.date(from: "1999/09/28 00:00:00") ?? Date()
-        }
-        
-    }
-    
-    func changeDateString(_ date: Date, _ dateFormatString: String = "yyyy/MM/dd HH:mm:ss")->String{
-        
-        let dateFormatter = DateFormatter()
-        
-        // フォーマット設定
-        dateFormatter.dateFormat = dateFormatString
-
-        // ロケール設定（端末の暦設定に引きづられないようにする）
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-
-        // タイムゾーン設定（端末設定によらず、どこの地域の時間帯なのかを指定する）
-        dateFormatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
-        
-        let str = dateFormatter.string(from: date)
-        
-        if str.isEmpty {
-            return changeDateString(Date())
-        } else {
-            return str
-        }
+        self.model.kakeiboModel.append(KakeiboModel(id: "case1", name: "セブンイレブン弁当", price: 500, createAt: MyTools().changeStringDate("2024/01/20 11:21:33"), updateAt: MyTools().changeStringDate("2024/01/20 11:21:33"), categoryId: "c1", check: false))
+        self.model.kakeiboModel.append(KakeiboModel(id: "case2", name: "ファミマ弁当", price: 300, createAt: MyTools().changeStringDate("2024/02/20 11:22:33"), updateAt: MyTools().changeStringDate("2024/02/20 11:22:33"), categoryId: "c2", check: false))
+        self.model.kakeiboModel.append(KakeiboModel(id: "case3", name: "白菜", price: 100, createAt: MyTools().changeStringDate("2024/02/20 11:23:33"), updateAt: MyTools().changeStringDate("2024/02/20 11:23:33"), categoryId: "c3", check: false))
+        self.model.kakeiboModel.append(KakeiboModel(id: "case4", name: "セブンイレブン弁当", price: 500, createAt: MyTools().changeStringDate("2024/03/20 11:24:33"), updateAt: MyTools().changeStringDate("2024/03/21 11:24:33"), categoryId: "c4", check: false))
+        self.model.kakeiboModel.append(KakeiboModel(id: "case5", name: "フレンチレストラン", price: 5000, createAt: MyTools().changeStringDate("2024/03/20 11:25:33"), updateAt: MyTools().changeStringDate("2024/03/20 11:25:33"), categoryId: "c5", check: false))
     }
 }
