5b0e8acfaa270024f6c01bb6bb76f366f018e4c0
me990928 <me990928@gmail.com>
2024-03-03 01:40:49 +0900

    NavigationLinkの書き方を推奨のものに変更

--- Kakeibo/SideBarGroup/SideBarView.swift
+++ Kakeibo/SideBarGroup/SideBarView.swift
@@ -10,31 +10,45 @@ import SwiftUI
 struct SideBarView: View {
     
     @ObservedObject var testVM: TestViewModel
-    @State var selectedMonth: String = ""
+//    @State var selectedMonth: [String] = MyTools().getYearDate(year:  (Int(MyTools().changeDateString(Date(), "yyyy")) ?? 2000))
     
     @State var year: Int =  (Int(MyTools().changeDateString(Date(), "yyyy")) ?? 2000)
     // 年のセレクト
     // 月のセレクタで分ける
+    @State var reView: Bool = false
     
     var body: some View {
         VStack{
             HStack{
-                Button(action: { year -= 1 }, label: {
+                Button(action: { 
+                    year -= 1
+                    testVM.model.selectedMonth = MyTools().getYearDate(year: year)
+                    self.reView.toggle()
+                }, label: {
                     Text("<")
                 })
                 Text(String(year))
-                Button(action: { year += 1 }, label: {
+                Button(action: { 
+                    year += 1
+                    testVM.model.selectedMonth = MyTools().getYearDate(year: year)
+                    self.reView.toggle()
+                }, label: {
                     Text(">")
                 })
             }
             List(){
-                NavigationLink("ALL") {
-                    ContentBarView(testVM: testVM)
+                
+                NavigationLink("ALL", value: "ALL")
+                
+                ForEach(testVM.model.selectedMonth, id: \.self){item in
+                    NavigationLink(item, value: item)
                 }
-                ForEach(testVM.model.kakeiboModel){item in
-                    NavigationLink(MyTools().changeDateString(item.createAt, "yyyy/MM")) {
-                        ContentBarView(testVM: testVM)
-                    }
+                
+            }.navigationDestination(for: String.self) { Hashable in
+                if Hashable == "ALL" {
+                    Text("ALL")
+                }else{
+                    ContentBarView(testVM: testVM, selectDate: Hashable)
                 }
             }
         }
