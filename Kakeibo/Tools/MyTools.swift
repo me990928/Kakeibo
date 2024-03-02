e670c5519e68a45f998269aef0dd0227d1db3b38
me990928 <me990928@gmail.com>
2024-03-03 01:42:23 +0900

    日付型のデータ操作メソッドの追加

--- Kakeibo/Tools/MyTools.swift
+++ Kakeibo/Tools/MyTools.swift
@@ -59,4 +59,68 @@ class MyTools {
             return str
         }
     }
+    
+    func getYearDate(year: Int)->[String] {
+
+        var dateString: [String] = []
+        
+        // DateFormatterの設定
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY/MM" // 出力形式
+
+        // Calendarインスタンスの取得
+        var calendar = Calendar.current
+        calendar.timeZone = TimeZone(identifier: "Asia/Tokyo")!
+        // 2023年の各月の最初の日を生成し、それを指定した形式で出力
+        for month in 1...12 {
+            var dateComponents = DateComponents()
+            dateComponents.year = year
+            dateComponents.month = month
+            dateComponents.day = 1
+            dateComponents.hour = 0
+            dateComponents.minute = 0
+            dateComponents.second = 0
+            
+            // DateComponentsからDateを生成
+            if let date = calendar.date(from: dateComponents) {
+                // Dateを指定した形式の文字列に変換
+                dateString.append( dateFormatter.string(from: date) )
+            } else {
+                dateString.append("1999/09")
+            }
+        }
+//        print(dateString)
+        return dateString
+    }
+    
+    func getDayDates(yearMonth: String) -> [String] {
+        var dateStrings: [String] = []
+
+        let inputDateFormatter = DateFormatter()
+        inputDateFormatter.dateFormat = "yyyy/MM"
+        inputDateFormatter.timeZone = TimeZone(identifier: "Asia/Tokyo") // UTCタイムゾーンを設定
+
+        if let date = inputDateFormatter.date(from: yearMonth) {
+            let calendar = Calendar.current
+            let range = calendar.range(of: .day, in: .month, for: date)!
+            
+            let outputDateFormatter = DateFormatter()
+            outputDateFormatter.dateFormat = "yyyy/MM/dd"
+            outputDateFormatter.timeZone = TimeZone(identifier: "Asia/Tokyo") // UTCタイムゾーンを設定
+            
+            for day in range {
+                if let dayDate = calendar.date(bySetting: .day, value: day, of: date) {
+                    let dateString = outputDateFormatter.string(from: dayDate)
+                    dateStrings.append(dateString)
+                    print(dateString)
+                }
+            }
+        } else {
+            print("Invalid date format")
+        }
+
+        print(dateStrings)
+        
+        return dateStrings
+    }
 }
