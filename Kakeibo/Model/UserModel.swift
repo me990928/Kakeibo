0736df2aef33938fd063508fd556a979a47026a1
me990928 <me990928@gmail.com>
2024-03-11 18:29:07 +0900

    SwiftDataのモデル変更

--- Kakeibo/Model/UserModel.swift
+++ Kakeibo/Model/UserModel.swift
@@ -12,12 +12,14 @@ import SwiftData
 final class Users {
     var uuid: String
     var id: String
+    var name: String
     var pass: String
     var auth: String
     
-    init(uuid: String, id: String, pass: String, auth: String) {
+    init(uuid: String, id: String, name: String, pass: String, auth: String) {
         self.uuid = uuid
         self.id = id
+        self.name = name
         self.pass = pass
         self.auth = auth
     }
