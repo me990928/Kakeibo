f403daa9b5eeea56b335edf22a2acaabe382e434
me990928 <me990928@gmail.com>
2024-03-11 18:29:42 +0900

    モデル変更

--- Kakeibo/Mneu/User/UserSheetModel.swift
+++ Kakeibo/Mneu/User/UserSheetModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct UserSheetModel {
+    var inputId: String = ""
+    var inputPass: String = ""
+    var inputName: String = ""
+    
+    var isLoading: Bool = false
+    
+    // ユーザ権限後で追加用(アップデートネタ)
+    let authority: [String] = ["root", "Familly", "User"]
+    var selectedAuthority: String = "User"
+}
